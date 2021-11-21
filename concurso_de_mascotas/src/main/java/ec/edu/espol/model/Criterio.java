f7e7ef3e999209fb6f09f5dedecff2c9dcafdb1e
jonjapib <jonjapib.espol.edu.ec>
2021-11-20 20:42:27 -0500

    estructura de la clase criterio y miembroJurado

--- concurso_de_mascotas/src/main/java/ec/edu/espol/model/Criterio.java
+++ concurso_de_mascotas/src/main/java/ec/edu/espol/model/Criterio.java
@@ -5,10 +5,88 @@
  */
 package ec.edu.espol.model;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author Pibaque Ponce
  */
 public class Criterio {
-    
+    private int idCriterio;
+    private String descripcion;
+    private ArrayList<Evaluacion> evaluaciones;
+    private int Concurso;
+
+    public Criterio(int idCriterio, String descripcion, int Concurso) {
+        this.idCriterio = idCriterio;
+        this.descripcion = descripcion;
+        this.Concurso = Concurso;
+        this.evaluaciones=new ArrayList<>();
+    }
+
+    public int getIdCriterio() {
+        return idCriterio;
+    }
+
+    public void setIdCriterio(int idCriterio) {
+        this.idCriterio = idCriterio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public ArrayList<Evaluacion> getEvaluaciones() {
+        return evaluaciones;
+    }
+
+    public void setEvaluaciones(ArrayList<Evaluacion> evaluaciones) {
+        this.evaluaciones = evaluaciones;
+    }
+
+    public int getConcurso() {
+        return Concurso;
+    }
+
+    public void setConcurso(int Concurso) {
+        this.Concurso = Concurso;
+    }
+  public Criterio nextCriterio(Scanner sc){
+  
+        return null;
+  
+  }
+  public void savefile(String File){
+  try(PrintWriter pw= new PrintWriter(new FileOutputStream(new File(File),true))){
+      System.out.println("");
+  }catch(Exception e){
+      System.out.println(e.getMessage());
+  }
+  }
+  public ArrayList<Criterio> ArrayFile(String File){
+      ArrayList<Criterio> criterios = new ArrayList<>();
+      try(Scanner sc =new Scanner(new File(File))){
+            while(sc.hasNextLine()){
+      String linea =sc.nextLine();
+      //
+      //
+            }
+      } catch(Exception e){
+          System.out.println(e.getMessage());
+      }  
+      return null;
+      
+  }
+  
+  
+  
 }
--- concurso_de_mascotas/src/main/java/ec/edu/espol/model/MiembroJurado.java
+++ concurso_de_mascotas/src/main/java/ec/edu/espol/model/MiembroJurado.java
@@ -5,10 +5,118 @@
  */
 package ec.edu.espol.model;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author Pibaque Ponce
  */
 public class MiembroJurado {
+    private int idMJurado;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String email;
+    private String perfil;
+    private ArrayList<Evaluacion> evaluacion;
+
+    public MiembroJurado(int idMJurado, String nombre, String apellido, String telefono, String email, String perfil) {
+        this.idMJurado = idMJurado;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.email = email;
+        this.perfil = perfil;
+    }
+
+    public int getIdMJurado() {
+        return idMJurado;
+    }
+
+    public void setIdMJurado(int idMJurado) {
+        this.idMJurado = idMJurado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(String perfil) {
+        this.perfil = perfil;
+    }
+
+    public ArrayList<Evaluacion> getEvaluacion() {
+        return evaluacion;
+    }
+
+    public void setEvaluacion(ArrayList<Evaluacion> evaluacion) {
+        this.evaluacion = evaluacion;
+    }
+    
+    public Criterio nextCriterio(Scanner sc){
+  
+        return null;
+  
+  }
+  public void savefile(String File){
+  try(PrintWriter pw= new PrintWriter(new FileOutputStream(new File(File),true))){
+      System.out.println("");
+  }catch(Exception e){
+      System.out.println(e.getMessage());
+  }
+  }
+  public ArrayList<Criterio> ArrayFile(String File){
+      ArrayList<Criterio> criterios = new ArrayList<>();
+      try(Scanner sc =new Scanner(new File(File))){
+            while(sc.hasNextLine()){
+      String linea =sc.nextLine();
+      //
+      //
+            }
+      } catch(Exception e){
+          System.out.println(e.getMessage());
+      }  
+      return null;
+      
+  }
+  
+    
+    
     
 }
