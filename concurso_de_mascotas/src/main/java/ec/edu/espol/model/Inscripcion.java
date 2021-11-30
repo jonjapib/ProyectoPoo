2d0bce626b6116172c3f610fb947d61e0368d64f
evangeline24 <atinajara@gmail.com>
2021-11-30 09:37:52 -0500

    inscripcion.1

--- concurso_de_mascotas/src/main/java/ec/edu/espol/model/Inscripcion.java
+++ concurso_de_mascotas/src/main/java/ec/edu/espol/model/Inscripcion.java
@@ -5,10 +5,40 @@
  */
 package ec.edu.espol.model;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author Pibaque Ponce
  */
 public class Inscripcion {
+    //Argumentos
+    private int id;
+    private int idMascota;
+    private Mascota mascota;
+    private int idConcurso;
+    private double valor;
+    private double descuento;
+    
+    //Método nextInscripcion
+    public Inscripcion nextInscripcion(Scanner sc){
+        
+        return null;
+        
+    }
+    
+    //Método saveFile
+    public void saveFile(String File){
+        
+    }
+    
+    //Método readFile
+    public ArrayList readFile(String File){
+        
+        return null;
+        
+    }
+  
     
 }
